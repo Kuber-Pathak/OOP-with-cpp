66add470d3124c3428ff92fc2f673754cbdc6513
Kuber-Pathak <kuber.211417@ncit.edu.np>
2023-02-08 22:06:16 +0545

    modified

--- Small Project/Rock_Paper_scissor_game.cpp
+++ Small Project/Rock_Paper_scissor_game.cpp
@@ -4,72 +4,98 @@ using namespace std;
 
 int main()
 {
-    int u;
-    srand(time(0));
-    int n = rand() % 3 + 1;
-    cout << "Enter :" << endl
-         << "1 for scissors" << endl
-         << "2 for rock" << endl
-         << "3 for paper" << endl;
-    cin >> u;
-    if (u == n)
+    int u, r, c = 0, p = 0;
+    cout << "Please select number of round u want to play :" << endl;
+    cin >> r;
+    for (int i = 1; i <= r; i++)
     {
-        if (u == 1)
-        {
-            cout << "You both picked scissors " << endl;
-            cout << "Draw" << endl;
-        }
-        else if (u == 2)
-        {
-            cout << "You both picked Rock " << endl;
-            cout << "Draw" << endl;
-        }
-        else if (u == 3)
-        {
-            cout << "You both picked paper " << endl;
-            cout << "Draw" << endl;
-        }
-    }
-    else if (u == 1 && n == 2)
-    {
-        cout << "You picked Scissors" << endl;
-        cout << "Computer picked rock" << endl;
-        cout << "computer wins" << endl;
-    }
-    else if (u == 1 && n == 3)
-    {
-        cout << "You picked Scissors" << endl;
-        cout << "Computer picked paper" << endl;
-        cout << "You win" << endl;
-    }
 
-    else if (u == 2 && n == 1)
-    {
-        cout << "You picked Rock" << endl;
-        cout << "Computer picked Scissors" << endl;
-        cout << "You win" << endl;
+        srand(time(0));
+        int n = rand() % 3 + 1;
+        cout << "Enter :" << endl
+             << "1 for scissors" << endl
+             << "2 for rock" << endl
+             << "3 for paper" << endl;
+        cin >> u;
+        if (u == n)
+        {
+            if (u == 1)
+            {
+                cout << "You both picked scissors " << endl;
+                cout << "Draw" << endl;
+                c++;
+                p++;
+            }
+            else if (u == 2)
+            {
+                cout << "You both picked Rock " << endl;
+                cout << "Draw" << endl;
+                c++;
+                p++;
+            }
+            else if (u == 3)
+            {
+                cout << "You both picked paper " << endl;
+                cout << "Draw" << endl;
+                c++;
+                p++;
+            }
+        }
+        else if (u == 1 && n == 2)
+        {
+            cout << "You picked Scissors" << endl;
+            cout << "Computer picked rock" << endl;
+            cout << "computer wins" << endl;
+            c++;
+        }
+        else if (u == 1 && n == 3)
+        {
+            cout << "You picked Scissors" << endl;
+            cout << "Computer picked paper" << endl;
+            cout << "You win" << endl;
+            p++;
+        }
+
+        else if (u == 2 && n == 1)
+        {
+            cout << "You picked Rock" << endl;
+            cout << "Computer picked Scissors" << endl;
+            cout << "You win" << endl;
+            p++;
+        }
+        else if (u == 2 && n == 3)
+        {
+            cout << "You picked Rock" << endl;
+            cout << "Computer picked paper" << endl;
+            cout << "computer wins" << endl;
+            c++;
+        }
+        else if (u == 3 && n == 1)
+        {
+            cout << "You picked Paper" << endl;
+            cout << "Computer picked Scissors" << endl;
+            cout << "computer wins" << endl;
+            c++;
+        }
+        else if (u == 3 && n == 2)
+        {
+            cout << "You picked Paper" << endl;
+            cout << "Computer picked rock" << endl;
+            cout << "You win" << endl;
+            p++;
+        }
+        else
+        {
+            cout << "invalid input" << endl;
+        }
     }
-    else if (u == 2 && n == 3)
+    if (p > c)
     {
-        cout << "You picked Rock" << endl;
-        cout << "Computer picked paper" << endl;
-        cout << "computer wins" << endl;
-    }
-    else if (u == 3 && n == 1)
-    {
-        cout << "You picked Paper" << endl;
-        cout << "Computer picked Scissors" << endl;
-        cout << "computer wins" << endl;
-    }
-    else if (u == 3 && n == 2)
-    {
-        cout << "You picked Paper" << endl;
-        cout << "Computer picked rock" << endl;
-        cout << "You win" << endl;
+        cout << "You won most of the rounds" << endl;
     }
     else
     {
-        cout << "invalid input" << endl;
+        cout << "Computer won most of the rounds" << endl;
     }
     return 0;
 }
