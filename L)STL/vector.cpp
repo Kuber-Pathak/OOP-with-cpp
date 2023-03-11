2e7b749a27b703c3f7a8ef3ead0ceee611990010
Kuber-Pathak <kuber.211417@ncit.edu.np>
2023-03-11 08:51:12 +0545

    modified

---
+++ L)STL/vector.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+template <class T>
+void display(vector<T> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    vector<int> vec1;
+    int element, size;
+    cout << "Enter the size of vector" << endl;
+    cin >> size;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter an element to add to this vector" << endl;
+        cin >> element;
+        vec1.push_back(element);
+    }
+    display(vec1);
+    return 0;
+}
